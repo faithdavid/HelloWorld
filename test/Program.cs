c23813bd90a6cfb41ecde4de0e93fba57da6e15b
faithdavid <faithdavidaregbesola@gmail.com>
2024-04-11 15:32:33 -0700

    new project for C#

---
+++ test/Program.cs
@@ -0,0 +1,74 @@
+﻿// improve renewal rate of subscriptions
+
+
+
+
+
+
+
+
+
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll1 == roll3) || (roll2 == roll3))
+// {
+//     Console.WriteLine("You rolled doubles! +2 bonus to total!");
+//     total += 2;
+// }
+
+// // to implement for if you rolled three
+
+// if ((roll1 == roll2) && (roll2 == roll3))
+// {
+//     Console.WriteLine("You rolled triples! +6 bonus to total!");
+//     total += 6;
+// }
+
+// if (total >= 15)
+// {
+//     Console.WriteLine("You Win!");
+// }
+
+// if (total < 15)
+// {
+//     Console.WriteLine("You Loose!");
+// }
+
+
+
+
+
+// int firstValue = 500;
+// int secondValue = 600;
+// int largerValue = Math.Max(firstValue, secondValue);
+
+// Console.WriteLine(largerValue);
+
+// Console.WriteLine("I'm steadily improving \"let's go! \". \n");
+
+// Random post = new();
+
+// int characters = post.Next(1, 81);
+
+// Console.Write($@"Generating new post. Chat MePT, hahaha:
+// New post is within acceptable character limit. Post character: {characters}");
+
+
+// new dice app
+// Exercise
+
+// Random dice = new();
+
+// int roll111 = dice.Next();
+// int roll112 = dice.Next(101);
+// int roll113 = dice.Next(50,101);
+
+// Console.WriteLine($"Roll 111: {roll111}, Roll 112: {roll112}, Roll 113: {roll113}");
